491aed4904d72761d136461bbc9341aac2417501
niklasinde <niklasinde@gmail.com>
2017-09-12 15:07:12 +0200

    Project 2 update

--- project2/project2.py
+++ project2/project2.py
@@ -7,38 +7,25 @@ Created on Tue Sep 12 12:18:24 2017
 """
 
 import scipy as sp
-
+import numpy as np
 from sympy.utilities.lambdify import lambdify
 import sympy as sm
+import matplotlib.pyplot as plt
 
 
 class bernsteinbasis:
     def __init__(self,deg,k):
-        
-        if deg > 0:
-            self.k = k
-            self.deg = deg
-            self.multiplebasis = False
-        
-        self.x = sm.Symbol("x")
-        self.basefuncsm = self.polybase()
-        self.basefunceval = lambdify(self.x, self.basefuncsm, modules=['numpy'])
 
-    def polybase(self):
-        Bvn = sp.special.binom(self.deg, k) * (self.x)**k * (1-self.x)**(self.deg-self.k)
+        self.k = k
+        self.deg = deg
+    
+    def __call__(self,x):
+        return(sp.special.binom(self.deg, self.k) * (x)**self.k * (1-x)**(self.deg-self.k))
         
-    def __call__(self,t):
-        return(self.basefunceval(t))
-        
-    def __add__(self,other):
-        
-        new_class = bernsteinbasis(other.deg,-1)
-        
-        new_ca
-        
-        add = self.basefuncsm + other.basefuncsm
-        return(add)
-        
+
+    
+
+
 
 
 
@@ -47,16 +34,44 @@ class bernstinepoly:
     def __init__(self,deg):
         
         self.deg = deg
-        self.polylist = self.polys()
+        self.basis = self.polys()
         
     def polys(self):
         """
         generates a list with all the bases.
         """
-        polylist = []
-        for i in range(len(deg)+1):
-            polylist.append(bernsteinbasis(self.deg),i)
+        basis = []
+        for k in range(self.deg+1):
+            basis.append(bernsteinbasis(self.deg,k))
+        return(basis)
             
+    def __call__(self,x):
+        
+        _sum = 0
+        for base in self.basis:
+            _sum += base(x)
             
-    def __call__(self,t):
-        pass        
+        return(_sum)
+        
+    def callbase(self,x,i):
+        return(self.basis[i](x))
+
+        
+x = np.linspace(0,1,50)
+
+bern = bernstinepoly(4)
+
+
+y = bern(x)
+        
+        
+     
+plt.plot(x,y)
+        
+        
+
+        
+        
+        
+        
+        
