abacfa133cb2bc0bc0bdec5109cfd442f2e12f13
Gustav Nygren <gustavnygren@users.noreply.github.com>
2017-09-12 15:11:52 +0200

    123

--- project2/project2.py
+++ project2/project2.py
@@ -15,27 +15,18 @@ import matplotlib.pyplot as plt
 
 class bernsteinbasis:
     def __init__(self,deg,k):
-
         self.k = k
         self.deg = deg
-    
+
     def __call__(self,x):
-        return(sp.special.binom(self.deg, self.k) * (x)**self.k * (1-x)**(self.deg-self.k))
-        
-
-    
-
-
-
-
-
+        return(sp.special.binom(self.deg, self.k)*
+               (x)**self.k*(1-x)**(self.deg-self.k))
 
 class bernstinepoly:
     def __init__(self,deg):
-        
         self.deg = deg
         self.basis = self.polys()
-        
+
     def polys(self):
         """
         generates a list with all the bases.
@@ -44,34 +35,21 @@ class bernstinepoly:
         for k in range(self.deg+1):
             basis.append(bernsteinbasis(self.deg,k))
         return(basis)
-            
+
     def __call__(self,x):
-        
+
         _sum = 0
         for base in self.basis:
             _sum += base(x)
-            
+
         return(_sum)
-        
+
     def callbase(self,x,i):
         return(self.basis[i](x))
 
-        
 x = np.linspace(0,1,50)
 
 bern = bernstinepoly(4)
 
-
 y = bern(x)
-        
-        
-     
 plt.plot(x,y)
-        
-        
-
-        
-        
-        
-        
-        
