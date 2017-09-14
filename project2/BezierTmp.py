a09e49126c985791f0a5487af6605741098b0aef
Gustav Nygren <gustavnygren@users.noreply.github.com>
2017-09-15 00:19:11 +0200

    Implemented subdivision

--- project2/BezierTmp.py
+++ project2/BezierTmp.py
@@ -19,63 +19,91 @@ class BernBasis:
     def __init__(self, degree:'int', iteration:'int'):
         self.n, self.i = degree, iteration
         assert self.n >= self.i, 'passed max iter.'
+        self.coeff = binom(self.n, self.i)
+        self.evaluate = self.__getB()
 
     def __call__(self, arg:'float') -> 'float':
+        return self.evaluate(arg)
+
+    def __getB(self) -> 'float':
         var = lambda x: x**self.i * (1 - x)**(self.n - self.i)
-        return binom(self.n, self.i)*var(arg)
+        return lambda arg: self.coeff*var(arg)
 
 class RecurBasis:
     '''Used for task 3, to mimic the
        computation of homework 1.'''
     def __init__(self, pts):
         self.pts, self.n = pts, len(pts)-1
-        self.Bezier = self._generateCurve()
+        self.b = self.__getCurve()
 
-    def _generateCurve(self) -> 'func':
+    def __getCurve(self) -> 'func':
         def b(i, k):
             if k == 0: return lambda _: self.pts[i, :]
             else: return lambda t: (1-t)*b(i, k-1)(t) + t*b(i+1, k-1)(t)
 
         return b(0, self.n)
 
-    def _render(self, nsp=100, colour='r', env=None) -> 'None':
-        values = array(list(self.Bezier(x) for x in linspace(0, 1, nsp)))
+    def subdivision(self, t=0.5):
+        def b(i, k):
+            if k == 0: return lambda _: self.pts[i, :]
+            else: return lambda t: (1-t)*b(i, k-1)(t) + t*b(i+1, k-1)(t)
+
+        pts1 = array(list(reversed(list(b(0, tmp)(t)
+                     for tmp in reversed(range(self.n+1))))))
+        pts2 = array(list(b(self.n - tmp, tmp)(t)
+                     for tmp in reversed(range(self.n+1))))
+
+        scatter(pts1[:, 0], pts1[:, 1], c='r')
+        scatter(pts2[:, 0], pts2[:, 1], c='b')
+
+        A1 = BernPoly(pts1)
+        A1.render([0,t], colour = 'r')
+        A2 = BernPoly(pts2)
+        A2.render([t, 1], colour = 'b')
+
+    def render(self, nsp=100, colour='r', env=None) -> 'None':
+        values = array(list(self.b(x) for x in linspace(0, 1, nsp)))
 
         plot(values[:, 0], values[:, 1], c=colour)
         if env:
+            LineSegment = lambda i, j: [self.pts[i, j], self.pts[i+1, j]]
             scatter(self.pts[:, 0], x[:, 1], c='k', alpha=0.5)
-            plot(list([self.pts[i, 0], self.pts[i+1, 0]] for i in range(self.n)),
-                 list([self.pts[i, 1], self.pts[i+1, 1]] for i in range(self.n)),
+            plot(list(LineSegment[i, 0] for i in range(self.n)),
+                 list(LineSegment[i, 1] for i in range(self.n)),
                  c='k', alpha=0.2)
 
 class BernPoly:
-    def __init__(self, pts:'vector'):
-        self.pts, self.n = pts, len(pts) - 1
-        self.basis = self._generateBasis()
-        self.px    = self._generatePolynomial()
+    def __init__(self, pts:'list/vector'):
+        self.pts, self.n = array(pts), len(pts) - 1
+        self.basis = self.__getBasis()
+        self.B     = self.__getPolynomial()
 
     def __call__(self, x) -> 'matrix':
-        return self.px(x)
+        return self.B(x)
 
-    def _generatePolynomial(self) -> 'func':
+    def __getPolynomial(self) -> 'func':
         return lambda x: sum(self.pts[i]*base(x)
                              for i, base in enumerate(self.basis))
 
-    def _render(self, domain=[0,1], nsp=100, colour='r', env=None) -> 'None':
+    def __getBasis(self) -> 'matrix':
+        return array(list(BernBasis(self.n, idx) for idx in range(self.n + 1)))
+
+    def render(self, domain=[0,1], nsp=100, colour='r', env=None) -> 'None':
         sample = linspace(domain[0], domain[1], nsp)
         u = lambda a: a
-        values = array(list(self.px(u(x))
+        values = array(list(self.B(u(x))
                        for x in linspace(0, 1, nsp)))
 
-        plot(sample, values[:, 1], c=colour)
+        plot(sample, values[:, 1], c=colour, alpha=0.5)
 
         if env:
+            LineSegment = lambda i, j: [self.pts[i, j], self.pts[i+1, j]]
             scatter(self.pts[:, 0], self.pts[:, 1], c='k', alpha=0.5)
-            plot(list([self.pts[i, 0], self.pts[i+1, 0]] for i in range(self.n)),
-                 list([self.pts[i, 1], self.pts[i+1, 1]] for i in range(self.n)),
+            plot(list(LineSegment(i, 0) for i in range(self.n)),
+                 list(LineSegment(i, 1) for i in range(self.n)),
                  c='k', alpha=0.2)
 
-    def _PlotBasisFuncs(self, pt=False) -> 'None':
+    def renderBasisFuncs(self, pt = False) -> 'None':
         tmp = self.basis.copy()
         sample = linspace(0, 1, 100)
         for i, p in enumerate(tmp):
@@ -86,37 +114,40 @@ class BernPoly:
 
         if pt: print(sum(base(pt) for base in tmp))
 
-    def _generateBasis(self) -> 'matrix':
-        return array(list(BernBasis(self.n, idx) for idx in range(self.n + 1)))
 
+if __name__=='__main__':
+    x = array([[0,0],
+               [0.2, 0.5],
+               [0.4, 0.2],
+               [0.7, -0.2],
+               [1, 0.1]])
+    '''task1'''
+    #P = BernPoly(x)
+    #P.renderBasisFuncs(0.2)
+    '''task2'''
+    pts3 = array([[0.05, 0.02], [0.1, 0.2],
+                 [0.2, -0.1], [0.3, 0],
+                 [0.4, 0.1], [0.7, 0.2]])
+    #@timeit
+    #def RecursiveMethod():
+    #    proj1 = RecurBasis(pts)
+    #    proj1.render()
+    #@timeit
+    #def BernsteinMethod():
+    #    proj2 = BernPoly(pts)
+    #    proj2.render()
 
-x = array([[0,0], [0.2, 0.5], [0.4, 0.2], [0.7, -0.2], [1, 0.1]])
+    #RecursiveMethod()
+    #BernsteinMethod()
+    '''task3'''
+    pts = array([[0, 0], [0.25, 0.5], [0.75, 0.5], [1, 0]])
+    B = BernPoly(pts)
+    B.render(domain=[0,1], env=True)
+    A = RecurBasis(pts)
+    A.subdivision()
 
-'''task1'''
-#P = BernPoly(x)
-#P._PlotBasisFuncs(0.2)
-'''task2'''
-pts = array([ [0.05, 0.02], [0.1, 0.2],
-              [0.2, -0.1], [0.3, 0],
-              [0.4, 0.1], [0.7, 0.2]])
-#@timeit
-#def RecursiveMethod():
-#    proj1 = RecurBasis(pts)
-#    proj1._render()
-#@timeit
-#def BernsteinMethod():
-#    proj2 = BernPoly(pts)
-#    proj2._render()
-
-#RecursiveMethod()
-#BernsteinMethod()
-
-'''task3'''
-A = BernPoly(pts)
-A._render(domain=[0,1], env=True)
-
-xlim(-0.2, 1.4)
-ylim(-0.2, 0.4)
-grid()
-#savefig('basisfuncs.pdf')
-show()
+    #xlim(-0.2, 1.4)
+    #ylim(-0.2, 0.4)
+    grid()
+    savefig('SubDivision.pdf')
+    show()
