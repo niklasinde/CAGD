cf052d63185847b0a074d6b8775ac216bcdcfb80
Gustav Nygren <gustavnygren@users.noreply.github.com>
2017-10-16 16:22:20 +0200

    task1_done

--- project6/coonspatch.py
+++ project6/coonspatch.py
@@ -46,7 +46,7 @@ class surface:
         '''
         self.b1, self.b2, self.b3, self.b4 = points1, points2, points3, points4
         self.domain = domain
-        _ = self.domain
+        _ = domain
         self.c0, self.c1, self.d0, self.d1 = Bezier(points1, domain=_),\
                                              Bezier(points2, domain=_),\
                                              Bezier(points3, domain=_),\
@@ -54,9 +54,8 @@ class surface:
         self.C = self.__run()
 
     def __run(self):
-        _p = lambda t: (self.domain[-1] - t)/self.domain[-1] if self.domain[-1] != 0 else 0
-        _q = lambda t: (t - self.domain[0])/(self.domain[-1] - self.domain[0])\
-                       if (self.domain[-1] - self.domain[0]) != 0 else 0
+        _p = lambda t: (1-t)
+        _q = lambda t: t
 
 
         Lc = lambda s, t: _p(t)*self.c1(s) + _q(t)*self.c0(s)
@@ -64,7 +63,7 @@ class surface:
         B  = lambda s, t: self.c1(0)*_p(s)*_p(t)\
                           + self.c1(1)*_q(s)*_p(t)\
                           + self.c0(0)*_p(s)*_q(s)\
-                          + self.c0(1)*_q(s)*_q(s)
+                          + self.c0(1)*_q(s)*_q(t)
 
         return lambda s, t: Lc(s, t) + Ld(s, t) - B(s, t)
 
@@ -85,29 +84,19 @@ class surface:
             ax.plot(xs = Y[:, 0], ys = Y[:, 1], zs = Y[:,2], alpha=0.5, c='k')
 
 
+        for bez in (self.c0, self.c1, self.d0, self.d1):
+            bez.render()
+
 
 p1 = array([ [0,0,0], [1,0,1], [2,0,1], [3,0,0] ])
 p2 = array([ [0,3,0], [1,3,1], [2,3,1], [3,3,0] ])
 p3 = array([ [0,0,0], [0,1,1], [0,2,-3], [0,3,0] ])
 p4 = array([ [3,0,0], [3,1,1], [3,2,1], [3,3,0] ])
 
-#p1 = array([ [0,0,0], [1/3,0,1/3], [2/3,0,1/3], [3/3,0,0] ])
-#p2 = array([ [0,3/3,0], [1/3,3/3,1/3], [2/3,3/3,1/3], [3/3,3/3,0] ])
-#p3 = array([ [0,0,0], [0,1/3,1/3], [0,2/3,-3/3], [0,3/3,0] ])
-#p4 = array([ [3/3,0,0], [3/3,1/3,1/3], [3/3,2/3,1/3], [3/3,3/3,0] ])
-
-
 lista = (p1, p2, p3, p4)
-domain = (0, 3)
+domain = (0, 1)
 
 C = surface(*lista, domain)
 C.render()
-
-
-
-
-for arg in lista:
-    C = Bezier(arg, domain )
-    C.render()
 
 plt.show()
